18b85d5dc4e14a34a3c9219c31477e168f36c08b
Daniel Krom <danielkrom@outlook.com>
2026-08-31 23:46:31 -0300

    Tratamento de excessão, produto não encontrado

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdEndpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Products.GetProductsById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+
+internal class GetProductsByIdEndpoint
+{
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.GetProductsById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+
+public class GetProductsByIdHandler
+    (IDocumentSession session, ILogger<GetProductsByIdHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsByIdHandler.Handle called with {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if(product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
+
+
