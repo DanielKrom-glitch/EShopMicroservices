05557f96209a18efa4c94ede7329207072e0ab0d
Daniel Krom <danielkrom@outlook.com>
2026-09-03 00:11:11 -0300

    Finalização do CRUD dos produtos

--- src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdEndpoint.cs
@@ -1,10 +1,25 @@
-﻿namespace Catalog.API.Products.GetProductsById;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
 
-public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+namespace Catalog.API.Products.GetProductsById;
 
-public record GetProductByIdResult(Product Product);
+public record GetProductByIdResponse(Product Product);
 
 
-internal class GetProductsByIdEndpoint
+internal class GetProductsByIdEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get a product by ID")
+        .WithDescription("Get a product by ID");
+    }
 }
