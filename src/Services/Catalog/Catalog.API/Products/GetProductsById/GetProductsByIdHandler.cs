ee74a11352bbdb71e9eaca30c1b9234462640275
Daniel Krom <danielkrom@outlook.com>
2026-09-09 00:29:05 -0300

    Tratamento das exceptions retornando a exception como um json hiper-informativo

--- src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductsById/GetProductsByIdHandler.cs
@@ -17,7 +17,7 @@ public class GetProductsByIdHandler
 
         if(product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(query.Id);
         }
 
         return new GetProductByIdResult(product);
